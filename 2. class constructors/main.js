0918d4b68dbdb8cdbb04dad8ff5f6d3c2ed111f8
Hasibul-99 <hasibul_uap@outlook.com>
2019-02-22 23:17:09 +0600

    Class constructors methods

--- 2. class constructors/main.js
+++ 2. class constructors/main.js
@@ -3,6 +3,12 @@ class User {
         this.email = email;
         this.name = name;
     }
+    logIn(){
+        console.log(this.email, "Log in");
+    }
+    logOut(){
+        console.log(this.email, 'Log out');
+    }
 }
 
 let userOne  = new User('hasan@mail.com', 'Hasan');
@@ -10,5 +16,8 @@ let userOne  = new User('hasan@mail.com', 'Hasan');
 let userTwo  = new User('tushar@gmail.com', 'Tushar');
 
 
-console.log("userOne", userOne);
-console.log("userTwo", userTwo);
+// console.log("userOne", userOne);
+// console.log("userTwo", userTwo);
+
+userOne.logIn();
+userTwo.logOut();
