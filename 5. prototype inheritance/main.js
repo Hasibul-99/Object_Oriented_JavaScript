ca471cdd1cf96b042c5e9a6897d2c685b6796c14
Hasibul-99 <hasibul_uap@outlook.com>
2019-02-24 09:54:28 +0600

    Prototype Inheritance

--- 5. prototype inheritance/main.js
+++ 5. prototype inheritance/main.js
@@ -15,9 +15,31 @@ User.prototype.logout = function() {
     console.log(this.name, 'has logged out');
 }
 
+function Admin(...args) {
+
+    // args = ['admin@gmail.com', 'Admin']
+
+    User.apply(this, args)
+    this.role = 'Admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u) {
+    users = users.filter(user =>  {
+        return user.email != u.email;
+    })
+}
+
 var userOne = new User('hasan@gmail.com', 'Hasan');
 
 var userTwo = new User('Tushar@gmail.com', 'Tushar');
 
-console.log("UserOne", userOne);
-userTwo.login();
+var admin = new Admin('admin@gmail.com', 'Admin');
+
+var users = [userOne, userTwo, admin];
+
+// console.log("UserOne", userOne);
+// userTwo.login();
+
+console.log(admin);
